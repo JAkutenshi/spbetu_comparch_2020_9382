7b5fd1b30e657586f77046d6ae4e28a9dbc1b8d0
Максим <maksim@MacBook-Pro-Maksim.local>
2020-10-22 10:11:11 +0300

    changes

--- subbotin/lab5/lab5.asm
+++ subbotin/lab5/lab5.asm
@@ -6,23 +6,47 @@ DATASG  SEGMENT  PARA 'Data'				;SEG DATA
 	KEEP_CS DW 0 ; для хранения сегмента
 	KEEP_IP DW 0 ; и смещения вектора прерывания
 	GREETING  DB 'Subbotin Maksim 9382 $'
-	HELLOWORLD DB 'Hello World!!! $'
+	COUNTER DW 0
+	crlf db 0ah, 0dh, '$'
 DATASG	ENDS								;ENDS DATA
 
 CODE      SEGMENT   						;SEG CODE
 ASSUME  DS:DataSG, CS:Code, SS:STACKSG
 
 WRITE_SOME  PROC FAR
+
 	PUSH AX   ; сохранение изменяемых регистров
+	PUSH DX
+
 	;<действия по обработке прерывания>
 	mov   AH,9 ;вызов того,
 	int   21h  ;что хранится в dx
-	
-	POP  AX    ;восстановление регистров
+
+	dec COUNTER
+
+	mov dx, COUNTER 
+	add dx,48
+
+    mov ah,2
+    int 21h
+
+    mov dx, OFFSET crlf
+    mov ah,9
+    int 21h
+
+	POP  DX    ;восстановление регистров
+	POP AX
+
 	MOV  AL, 20H
 	OUT  20H,AL
 	IRET
+
+	ST_SS DW 0000
+	ST_SP DW 0000
+	INT_STACK DW 20 DUP(0)
+
 WRITE_SOME  ENDP
+
 
 	
 	
@@ -36,22 +60,24 @@ Main      PROC  FAR
 	MOV  KEEP_IP, BX  ; запоминание смещения
 	MOV  KEEP_CS, ES  ; и сегмента вектора прерывания
 
+	CLI
 	PUSH DS
-	MOV  DX, OFFSET WRITE_SOME ; смещение для процедуры в DX
+	MOV DX, OFFSET WRITE_SOME
 	MOV  AX, SEG WRITE_SOME    ; сегмент процедуры
 	MOV  DS, AX          ; помещаем в DS
 	MOV  AH, 25H         ; функция установки вектора
 	MOV  AL, 1CH         ; номер вектора
 	INT  21H             ; меняем прерывание
 	POP  DS
+	STI
 	
-	
+
 	mov   DX, OFFSET GREETING ;так как наше переопределенное прерывние выводит строку, запишем в dx то, что надо вывести
-	int 1Ch ;вызываем наше переопределенное прерывание
+	mov COUNTER,10
 
-	mov   DX, OFFSET HELLOWORLD
-	int 1Ch
-
+	count_loop:
+		cmp COUNTER,0
+		jnz count_loop
 
 	
 	CLI
