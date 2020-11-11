581cd197ccf87a5a6a057d776b815281200d9c23
alexundertable <akodukovich@mail.ru>
2020-11-11 18:08:48 +0300

    lab5 fix

--- kodukov/lab5/func.asm
+++ kodukov/lab5/func.asm
@@ -6,6 +6,8 @@ DATASG  SEGMENT  PARA 'Data'; SEG DATA
   KEEP_CS DW 0; для хранения сегмента
   KEEP_IP DW 0; и смещения вектора прерывания
   GREETING  DB 'Kodukov Aleksandr 9382 $'
+  COUNTER1 DW 0
+  COUNTER2 DW 0
   crlf db 0ah, 0dh, '$'
 DATASG	ENDS; ENDS DATA
 
@@ -14,6 +16,25 @@ ASSUME DS:DataSG, CS:Code, SS:STACKSG
 
 INTER_TIMER PROC FAR
 
+  jmp s
+  ST_SS DW 0000
+  ST_SP DW 0000
+  ST_AX DW 0000
+  INT_STACK DW 20 DUP(0)
+
+  s:
+  mov ST_SP,SP ; cохраняю SP
+
+  mov ST_AX,AX
+
+  mov AX,SS
+  mov ST_SS,AX ; сохраняю SS
+
+  mov AX,INT_STACK
+  mov SS,AX ;новый стек
+
+  mov AX,ST_AX
+
   PUSH AX; сохранение изменяемых регистров
   PUSH DX
 
@@ -21,12 +42,42 @@ INTER_TIMER PROC FAR
   MOV AH, 9; вызов того,
   INT 21H; что хранится в dx
 
+  INC COUNTER1
+
+  cmp COUNTER1, 10
+  jl less1
+  INC COUNTER2
+  MOV COUNTER1, 0
+  less1:
+  MOV DX, COUNTER2
+  ADD DX, 48  
+  MOV ah,2
+  INT 21h
+  MOV DX, COUNTER1  
+  ADD DX, 48
+  MOV ah,2
+  INT 21h
+  cmp COUNTER2, 10
+  jl less2
+  MOV COUNTER1, 0
+  MOV COUNTER2, 0
+  less2:
+
   MOV DX, OFFSET crlf
   MOV AH, 9
   INT 21H
 
   POP DX; восстановление регистров
   POP AX
+
+  mov ST_AX,AX
+
+  mov AX,ST_SS ; восстанавливаю SS
+  mov SS,AX
+
+  mov SP,ST_SP ; восстанавливаю SP
+
+  mov AX,ST_AX
 
   MOV AL, 20H
   OUT 20H, AL
@@ -51,21 +102,28 @@ Main PROC FAR
   MOV AX, SEG INTER_TIMER; сегмент процедуры
   MOV DS, AX; помещаем в DS
   MOV AH, 25H; функция установки вектора
-  MOV AL, 08H; номер вектора
+  MOV AL, 23H; номер вектора
   INT 21H; меняем прерывание
   POP DS
   STI	
+  MOV DX, OFFSET GREETING;  
+  
+  ;esc_loop:
+  ;mov ah, 10h
+  ;int 16h
+  ;cmp al,27
+  ;jz EXIT
+  ;next:
+  ;loop esc_loop
 
-  MOV DX, OFFSET GREETING; помещаем строку в DS
-  INT 08h
-	
+  EXIT:
   CLI
   PUSH DS
   MOV DX, KEEP_IP
   MOV AX, KEEP_CS
   MOV DS, AX
   MOV AH, 25H
-  MOV AL, 08H
+  MOV AL, 23H
   INT 21H; восстанавливаем старый вектор прерывания
   POP DS
   STI
@@ -74,5 +132,6 @@ Main PROC FAR
   INT 21H
  
 Main ENDP
+
 CODE ENDS
 END Main
