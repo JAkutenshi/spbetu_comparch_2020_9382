d7daff7ec85a670821d752a0260ffd978d764547
vikdema <viktordeminoff@gmail.com>
2020-12-09 00:56:01 +0300

    demin_lab6

---
+++ demin/lab6/main.cpp
@@ -0,0 +1,105 @@
+//
+
+#include <iostream>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+extern "C" {
+	void ASM_FUN(int* LGrIn, int* array, int* res, int NInt,int NumRanDat);
+}
+
+
+int main()
+{
+	int NumRanDat = 0;//Длина массива числе
+	int Xmin = 0;//левая граница
+	int Xmax = 0;//правая границв
+	int NInt = 0;//количество интервалов 
+	int* LGrInt = nullptr;// массив левых границ интервалов
+	int* arr = nullptr;// массив целых чисел
+	int* res = nullptr;// результирующий массив
+
+	cout << "Enter the length of the array\n";
+	cin >> NumRanDat;
+	while (1) {
+		if (NumRanDat > 0 && NumRanDat <= 16 * 1024) {
+			break;
+		}
+		cout << "Wrong: try again\n";
+		cout << "Enter the length of the array\n";
+		cin >> NumRanDat;
+	}
+	arr = new int[NumRanDat];
+	cout << "Enter the bottom border\n";
+	cin >> Xmin;
+	cout << "Enter the upper  border\n";
+	cin >> Xmax;
+
+	cout << "Enter number of intervals\n";
+	cin >> NInt;
+
+	while (1) {
+		if (NInt > 0 && NInt <= 24) {
+			break;
+		}
+		cout << "Wrong: try again\n";
+		cout << "Enter number of intervals\n";
+		cin >> NInt;
+	}
+	LGrInt = new int[NInt];
+	res = new int[NInt];
+	cout << "Enter " << NInt - 2 << "lintervals(!=Xmin&&!=Xmax)\n";
+	for (int i = 1; i < NInt - 1; i++)
+	{
+		cin >> LGrInt[i];
+		while (1) {
+			if (LGrInt[i] >= Xmin && LGrInt[i] <= Xmax) {
+				break;
+			}
+			cout << "Wrong: try again\n";
+			cin >> LGrInt[i];
+		}
+	}
+	LGrInt[0] = Xmin;
+	LGrInt[NInt - 1] = Xmax;
+
+	//дискретная равномерное распределение
+	std::random_device                  rand_dev;
+	std::mt19937                        generator(rand_dev());
+	std::uniform_int_distribution<int>  distr(Xmin, Xmax);
+	for (int i = 0; i < NumRanDat; i++)
+	{
+		arr[i] = distr(generator);
+	}
+	for (int i = 0; i < NInt; i++)
+	{
+		res[i] = 0;
+	}
+
+	//вызов ассемблерной функции
+	ASM_FUN(LGrInt, arr, res,  NInt,NumRanDat);
+
+	std::ofstream file;
+	file.open("out.txt");
+
+	for (int i = 0; i < NumRanDat; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n";
+
+	for (int i = 0; i < NInt-1; i++)
+	{
+		cout<<"№" << i << " (" << LGrInt[i] << " ," << LGrInt[i+1]<<"): " << res[i] << "\n";
+	}
+	for (int i = 0; i < NInt-1; i++)
+	{
+		file << i << "  " << LGrInt[i] << "  " << res[i]<<"\n";
+	}
+
+	return 0;
+}
+
