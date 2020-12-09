5d8221955a5762944c737b3850742caab694565b
BabylonExile <webhead473@gmail.com>
2020-12-09 16:41:18 +0300

    Mikhailov_Dmitry_lab6

---
+++ mikhailov/lab6/lab_6_1.asm
@@ -0,0 +1,18 @@
+.MODEL flat, C 
+.CODE 
+func1 PROC C random_numbers : ptr dword, one_step_intervals_dist : ptr dword, number_of_random_numbers : dword, x_min : dword 
+; Данная функция строит единичное распределение. 
+mov ecx, 0; инициалиируем счетчик 
+mov esi, random_numbers; Устанавливаем указатель на входные данные 
+mov edi, one_step_intervals_dist; Устанавливаем указатель на выходные данные 
+start_func1: 
+; Начало цикла работы программы 
+mov eax, dword ptr [esi+ecx*4]; Считываем значение из входной строки 
+sub eax, x_min; вычисляем адрес, на котором находятся его смещения в массиве 
+inc dword ptr [edi+eax*4]; 
+inc ecx; 
+cmp ecx, number_of_random_numbers; 
+jl start_func1; 
+ret 
+func1 ENDP 
+end
