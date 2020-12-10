83ac00316abb01ceee59962248f1492ba2c29532
Роман Павлов <55055449+Crunchy0@users.noreply.github.com>
2020-12-10 15:42:20 +0300

    add_decimal_input

--- pavlov/lab7/lab7.asm
+++ pavlov/lab7/lab7.asm
@@ -3,14 +3,16 @@ stack segment stack
 stack ends
 
 data segment
+	choice db '0 - ����筮� �᫮, 1 - �����筮� �᫮', 13, 10, '$'
+	product dw 4 dup(0)
 	origin db 33, ?, 33 dup(0)
 	result db 33 dup(0)
+	error db "incorrect number$"
 data ends
 
 additional segment
 	assume ds:data, cs:additional
 	strToInt proc far
-
 		push ax
 		mov ax, data
 		mov ds, ax
@@ -90,13 +92,81 @@ additional segment
 		push bx
 		push cx
 		ret
-	
-	error db "incorrect number$"
+
 	strToInt endp
 additional ends
 
 code segment
 	assume ds:data, cs:code, ss:stack
+	
+	strToInt10 proc near
+	
+		push ax
+		mov ax, data
+		mov ds, ax
+		pop ax
+
+		xor cx, cx
+		mov ah,0ah
+		mov dx,offset origin		; Считывание строки и запись её в буфер, перевод на новую строку
+		int 21h
+		mov dl,0ah
+		mov ah,02
+		int 21h
+		
+		mov si,offset origin+2
+		xor dx, dx
+		xor ax, ax				; Готовим регистры для записи: ax = 0 , bx = 10 - основание СС
+		mov bx,10
+		
+		transform:
+		mov cl,[si]				; Проверка на последний символ
+		cmp cl,0dh
+		jz fin10
+		
+
+		cmp cl,'0'				; Проверка на соответствие цифре
+		jb err10
+		cmp cl,'9'
+		ja err10
+	 
+		sub cl,'0'				; Перевод из кода символа в цифру, домножение на 10, прибавление в конец
+		
+		cmp dx, 0
+		jg omg
+		
+		mul bx
+		add ax,cx
+		inc si
+		
+		jmp transform
+		
+		omg:
+		push dx
+		mul bx
+		mov product[2], dx
+		mov product[4], ax
+		pop ax
+		mul bx
+		add product, dx
+		adc product[2], ax
+		mov dx, product[2]
+		mov ax, product[4]
+		adc ax, cx
+		inc si
+		jmp transform
+	 
+		err10:
+		mov dx, offset error	; Ошибка (если не цифра), выход
+		mov ah,09
+		int 21h
+		int 20h
+		
+		fin10:
+		ret
+	
+	strToInt10 endp
+	
 	intToStr proc near
 		push ax
 		push bx
@@ -157,15 +227,44 @@ code segment
 		push ds
 		push ax
 		
-		mov ax, offset data
+		mov ax, data
 		mov ds, ax
 		
-		call es:strToInt		; Ввод числа
-		assume cs:code
+		mov dx, offset choice
+		mov ah, 9
+		int 21h
 		
-		pop dx					; Получение числа из стека
+		entering:
+		
+		mov ah, 1
+		int 21h
+		
+		push ax
+		
+		mov ah, 2
+		mov dl, 10
+		int 21h
+		mov dl, 13
+		int 21h
+		
 		pop ax
 		
+		cmp al, 30h
+		jne decimal
+		
+		call strToInt			; Ввод числа
+		;assume cs: code		
+		pop dx					; Получение числа из стека
+		pop ax
+		jmp toStr
+		
+		decimal:
+		cmp al, 31h
+		jne entering
+		
+		call strToInt10		; Ввод числа	
+		
+		toStr:
 		call intToStr			; Запись числа в виде строки
 		
 		mov dx, offset result
